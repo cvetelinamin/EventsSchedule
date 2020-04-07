bd751e8d5fd6a1961486745d75c4fc263a11dca4
cvetelinamin <cvetelina.min.georgieva@gmail.com>
2020-04-08 00:19:22 +0300

    Added views for editing and listing of news.

--- Web/EventsSchedule.Web.ViewModels/News/NewsShortViewModel.cs
+++ Web/EventsSchedule.Web.ViewModels/News/NewsShortViewModel.cs
@@ -4,9 +4,13 @@
 
     public class NewsShortViewModel
     {
+        public string Id { get; set; }
+
         public string Title { get; set; }
 
         public string Content { get; set; }
+
+        public string ShortContent => this.Content.Substring(0, 200) + "...";
 
         public DateTime CreatedOn { get; set; }
     }
